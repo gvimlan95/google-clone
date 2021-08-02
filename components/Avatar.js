05e293d376265dcd64b57d94c24115d36fbf1c15
Vimlan G <gvimlan95@gmail.com>
2021-08-02 21:54:06 +0800

    avatar bug fixed 2

--- components/Avatar.js
+++ components/Avatar.js
@@ -3,7 +3,6 @@ import Image from 'next/image';
 function Avatar({ url }) {
 	return (
 		<Image
-			loading="lazy"
 			className="cursor-pointer h-10 rounded-full transition duration-150 transform hover:scale-110"
 			src={url}
 			alt="profile pic"
