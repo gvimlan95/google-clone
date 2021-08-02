a8ab2a86ecaeedcffc622281919414cf39ea33aa
Vimlan G <gvimlan95@gmail.com>
2021-08-02 20:22:49 +0800

    avatar removed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ["www.google.co.uk"],
-  },
-}
+	reactStrictMode: true,
+	images: {
+		domains: ['www.google.co.uk', 'https://avatars.githubusercontent.com'],
+	},
+};
